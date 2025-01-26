e11151142bdc3eab0b86c39a280f8773a6f37fc9
kozae00 <rhwogus777@gmail.com>
2025-01-26 14:17:16 +0900

    init

---
+++ src/main/java/com/example/jump2sbb/Jump2sbbApplication.java
@@ -0,0 +1,13 @@
+package com.example.jump2sbb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jump2sbbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jump2sbbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jump2sbb/Jump2sbbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jump2sbb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jump2sbbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
