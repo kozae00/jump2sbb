32de2b2c7676d84cb62be36d396f132ebdf7b26a
kozae00 <rhwogus777@gmail.com>
2025-02-08 15:17:46 +0900

    findAll

--- src/test/java/com/example/jump2sbb/Jump2sbbApplicationTests.java
+++ src/test/java/com/example/jump2sbb/Jump2sbbApplicationTests.java
@@ -1,13 +1,38 @@
 package com.example.jump2sbb;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class Jump2sbbApplicationTests {
 
+	@Autowired
+	private QuestionRepository questionRepository;
+
 	@Test
-	void contextLoads() {
+	void testJpa() {
+//		Question q1 = new Question();
+//		q1.setSubject("sbb가 무엇인가요?");
+//		q1.setContent("sbb에 대해서 알고 싶습니다.");
+//		q1.setCreateDate(LocalDateTime.now());
+//		this.questionRepository.save(q1);
+//
+//		Question q2 = new Question();
+//		q2.setSubject("스프링부트 모델 질문입니다.");
+//		q2.setContent("id는 자동으로 생성되나요?");
+//		q2.setCreateDate(LocalDateTime.now());
+//		this.questionRepository.save(q2);
+
+		List<Question> all = this.questionRepository.findAll();
+		assertEquals(4, all.size());
+
+		Question q = all.get(0);
+		assertEquals("sbb가 무엇인가요?", q.getSubject());
 	}
 
 }
