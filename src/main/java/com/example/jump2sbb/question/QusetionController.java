8cb9f3c4cd6c0543e1ca530612247534ac0d75ad
kozae00 <rhwogus777@gmail.com>
2025-02-08 16:22:03 +0900

    question_detail

--- src/main/java/com/example/jump2sbb/question/QusetionController.java
+++ src/main/java/com/example/jump2sbb/question/QusetionController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -21,5 +22,10 @@ public class QusetionController {
         model.addAttribute("quetionList", questionList);
 
         return "question_list";
+    }
+
+    @GetMapping(value = "/question/list/{id}")
+    public String detail(Model model, @PathVariable("id") Integer id) {
+        return "question_detail";
     }
 }
