64c97fe2071ea56458a85456293a89ecf296abe1
kozae00 <rhwogus777@gmail.com>
2025-02-08 15:24:18 +0900

    findById, findBySubject

--- src/test/java/com/example/jump2sbb/Jump2sbbApplicationTests.java
+++ src/test/java/com/example/jump2sbb/Jump2sbbApplicationTests.java
@@ -1,10 +1,13 @@
 package com.example.jump2sbb;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -15,24 +18,46 @@ class Jump2sbbApplicationTests {
 	private QuestionRepository questionRepository;
 
 	@Test
-	void testJpa() {
-//		Question q1 = new Question();
-//		q1.setSubject("sbb가 무엇인가요?");
-//		q1.setContent("sbb에 대해서 알고 싶습니다.");
-//		q1.setCreateDate(LocalDateTime.now());
-//		this.questionRepository.save(q1);
-//
-//		Question q2 = new Question();
-//		q2.setSubject("스프링부트 모델 질문입니다.");
-//		q2.setContent("id는 자동으로 생성되나요?");
-//		q2.setCreateDate(LocalDateTime.now());
-//		this.questionRepository.save(q2);
+	@DisplayName("질문 생성")
+	void testJpa1() {
+		Question q1 = new Question();
+		q1.setSubject("sbb가 무엇인가요?");
+		q1.setContent("sbb에 대해서 알고 싶습니다.");
+		q1.setCreateDate(LocalDateTime.now());
+		this.questionRepository.save(q1);  // 첫번째 질문 저장
 
+		Question q2 = new Question();
+		q2.setSubject("스프링부트 모델 질문입니다.");
+		q2.setContent("id는 자동으로 생성되나요?");
+		q2.setCreateDate(LocalDateTime.now());
+		this.questionRepository.save(q2);  // 두번째 질문 저장
+	}
+
+	@Test
+	@DisplayName("findAll - 전체 조회")
+	void testJpa2() {
 		List<Question> all = this.questionRepository.findAll();
-		assertEquals(4, all.size());
+		assertEquals(2, all.size());
 
 		Question q = all.get(0);
 		assertEquals("sbb가 무엇인가요?", q.getSubject());
+	}
+
+	@Test
+	@DisplayName("findById - id값으로 조회")
+	void testJpa3() {
+		Optional<Question> oq = this.questionRepository.findById(1);
+		if(oq.isPresent()) {
+			Question q = oq.get();
+			assertEquals("sbb가 무엇인가요?", q.getSubject());
+		}
+	}
+
+	@Test
+	@DisplayName("findBySubject")
+	void testJpa4() {
+		Question q = this.questionRepository.findBySubject("sbb가 무엇인가요?");
+		assertEquals(1, q.getId());
 	}
 
 }
