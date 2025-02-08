bf1f448465370b82223f5e3b8d6c978c58859867
kozae00 <rhwogus777@gmail.com>
2025-02-08 16:03:01 +0900

    Question Controller

---
+++ src/main/java/com/example/jump2sbb/question/QusetionController.java
@@ -0,0 +1,15 @@
+package com.example.jump2sbb.question;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class QusetionController {
+
+    @GetMapping("/question/list")
+    @ResponseBody
+    public String list() {
+        return "question list";
+    }
+}
