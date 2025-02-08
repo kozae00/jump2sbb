81f8317d288d90b0224ceb1c4abb757ba9a5df04
kozae00 <rhwogus777@gmail.com>
2025-02-08 15:48:17 +0900

    Transactional

--- src/test/java/com/example/jump2sbb/Jump2sbbApplicationTests.java
+++ src/test/java/com/example/jump2sbb/Jump2sbbApplicationTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -118,10 +119,24 @@ class Jump2sbbApplicationTests {
 
 	@Test
 	@DisplayName("findById - Answer")
-	void testJpa() {
+	void testJpa10() {
 		Optional<Answer> oa = this.answerRepository.findById(1);
 		assertTrue(oa.isPresent());
 		Answer a = oa.get();
 		assertEquals(2, a.getQuestion().getId());
+	}
+
+	@Test
+	@Transactional
+	@DisplayName("질문 데이터에서 답변 데이터를 찾기")
+	void testJpa11() {
+		Optional<Question> oq = this.questionRepository.findById(2);
+		assertTrue(oq.isPresent());
+		Question q = oq.get();
+
+		List<Answer> answerList = q.getAnswerList();
+
+		assertEquals(1, answerList.size());
+		assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
 	}
 }
