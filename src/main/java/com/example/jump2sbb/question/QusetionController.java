376d599710167365b1be8a9a8e83f46f17320cb8
kozae00 <rhwogus777@gmail.com>
2025-02-08 16:12:46 +0900

    using to Model

--- src/main/java/com/example/jump2sbb/question/QusetionController.java
+++ src/main/java/com/example/jump2sbb/question/QusetionController.java
@@ -1,13 +1,25 @@
 package com.example.jump2sbb.question;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @Controller
 public class QusetionController {
 
+    private final QuestionRepository questionRepository;
+
     @GetMapping("/question/list")
-    public String list() {
+    public String list(Model model) {
+
+        List<Question> questionList = this.questionRepository.findAll();
+
+        model.addAttribute("quetionList", questionList);
+
         return "question_list";
     }
 }
